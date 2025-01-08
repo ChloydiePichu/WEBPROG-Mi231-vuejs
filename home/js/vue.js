cd46193863d0c9d100aaa7db4ddff9c0f2ea9019
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-01-08 23:41:06 +0800

    Added previous complete work

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,3 +1,6 @@
+const buttons = document.querySelectorAll('.button-container button');
+const displayBox = document.getElementById('display-box');
+const contentElements = {}; 
   const app = Vue.createApp({
    data() {
     return {
@@ -7,3 +10,17 @@
   })
   
   app.mount('#app')
+
+document.querySelectorAll('.hidden-content').forEach(element => {
+    contentElements[element.id] = element.innerHTML;
+    element.remove();
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const targetId = button.dataset.target;
+        if (contentElements[targetId]) {
+            displayBox.innerHTML = contentElements[targetId];
+        }
+    });
+});
