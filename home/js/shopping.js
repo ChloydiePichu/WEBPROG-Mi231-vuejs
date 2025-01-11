2bcff819ef49771af306b84707296bb54aafcedd
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-01-11 22:32:54 +0800

    fixed code

--- home/js/shopping.js
+++ home/js/shopping.js
@@ -1,20 +1,20 @@
 const app2 = Vue.createApp({
   data() {
-   return {
-     manyFoods: [
-       {name: 'Burrito', url: 'https://www.w3schools.com/vue/img_burrito.svg'},
-       {name: 'Salad', url: 'https://www.w3schools.com/vue/img_salad.svg'},
-       {name: 'Cake', url: 'https://www.w3schools.com/vue/img_cake.svg'},
-       {name: 'Soup', url: 'https://www.w3schools.com/vue/img_soup.svg'},
-       {name: 'Fish', url: 'https://www.w3schools.com/vue/img_fish.svg'},
-       {name: 'Pizza', url: 'https://www.w3schools.com/vue/img_pizza.svg'},
-       {name: 'Rice', url: 'https://www.w3schools.com/vue/img_rice.svg'}
-     ]
-   }
-  }
- })
+    return {
+      manyFoods: [
+        { name: 'Burrito', url: 'https://www.w3schools.com/vue/img_burrito.svg' },
+        { name: 'Salad', url: 'https://www.w3schools.com/vue/img_salad.svg' },
+        { name: 'Cake', url: 'https://www.w3schools.com/vue/img_cake.svg' },
+        { name: 'Soup', url: 'https://www.w3schools.com/vue/img_soup.svg' },
+        { name: 'Fish', url: 'https://www.w3schools.com/vue/img_fish.svg' },
+        { name: 'Pizza', url: 'https://www.w3schools.com/vue/img_pizza.svg' },
+        { name: 'Rice', url: 'https://www.w3schools.com/vue/img_rice.svg' },
+      ],
+    };
+  },
+});
 
- app2.mount('#app2')
+app2.mount('#app2');
 
 const app3 = Vue.createApp({
   data() {
@@ -25,23 +25,25 @@ const app3 = Vue.createApp({
       shoppingList: [
         { name: 'Tomatoes', number: 5, important: false, found: false },
         { name: 'Bread', number: 1, important: false, found: false },
-        { name: 'Soap', number: 1, important: true, found: true }
-      ]
-    }
+        { name: 'Soap', number: 1, important: true, found: true },
+      ],
+    };
   },
   methods: {
-    addItem(){
-      let item = {
+    addItem() {
+      const newItem = {
         name: this.itemName,
         number: this.itemNumber,
         important: this.itemImportant,
-        found: false
-      }
-      this.shoppingList.push(item)
-      this.itemName = null
-      this.itemNumber = null  
-      this.itemImportant = false
-    }
-  }
-})
-app3.mount('#app3')
+        found: false,
+      };
+      this.shoppingList.push(newItem);
+
+      this.itemName = null;
+      this.itemNumber = null;
+      this.itemImportant = false;
+    },
+  },
+});
+
+app3.mount('#app3');
