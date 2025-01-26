203a6af377eb1f14923ae43c979d2a4f5eb8c291
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-01-27 02:05:59 +0800

    fxed

--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -22,5 +22,5 @@ export default {
         display: inline-block;
         margin: 10px;
         padding: 10px;
-        background-color: lightyellow;
+        background-color: lightgreen;
     }</style>          
