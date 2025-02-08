a1ac26661055a39084ef1941c4c4bd4d81ddab70
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-02-08 14:48:05 +0000

    fixed color

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -22,11 +22,5 @@
   </script>
 
 <style>
-    #app > div {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightblue;
-    }
+    
 </style>
