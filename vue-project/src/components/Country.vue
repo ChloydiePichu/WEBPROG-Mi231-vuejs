97833d26a618e878a54ecc43c32785c13b83a3f8
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-02-08 14:16:31 +0000

    update

--- vue-project/src/components/Country.vue
+++ vue-project/src/components/Country.vue
@@ -16,3 +16,19 @@
         background-color: lightblue;
     }
 </style>
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+  </script>
