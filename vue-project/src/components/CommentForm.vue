42d06f4040c1eb7d7ef58144c861f7fd50f36b13
Chloyd Lechico <137134954+ChloydiePichu@users.noreply.github.com>
2025-02-08 14:38:48 +0000

    fix

--- vue-project/src/components/CommentForm.vue
+++ vue-project/src/components/CommentForm.vue
@@ -27,8 +27,6 @@
   const submissionStatus = ref(null);
   
   // Your Supabase URL and Key - IMPORTANT!
-  const supabaseUrl = 'https://asbkzrawjnszfkhokhhk.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzYmt6cmF3am5zemZraG9raGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0OTQwMjMsImV4cCI6MjA1NDA3MDAyM30.0HEzkHO3Ew-nKVNQw7y5We7_tWay4eSfGUHzaDdM4XA';
   const tableName = 'comments'; // Name of your Supabase table
   
   const supabase = createClient(supabaseUrl, supabaseKey);
